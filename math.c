f6f2b1f7727d7255cbcd14e6074eca0896624d58
pavishree <pypsa2014@gmail.com>
2019-03-03 12:39:31 +0530

    dded cos and tan function

--- math.c
+++ math.c
@@ -4,7 +4,17 @@ void sin()
 {
 printf("we are calling  sin function");
 }
+void cos()
+{
+printf("we are calling  cos function");
+}
+void tan()
+{
+printf("we are calling  tan function");
+}
 void main()
 {
 sin();
+cos();
+tan();
 }
